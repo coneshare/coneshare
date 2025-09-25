7ab4d04fbcec4e5e1f3256eded5b59d07afe0452
xiez <xiez1989@gmail.com>
2025-09-25 17:45:53 +0800

    fix: fix UI issue

--- frontend/src/pages/DocumentsPage.jsx
+++ frontend/src/pages/DocumentsPage.jsx
@@ -272,7 +272,7 @@ function DocumentsPage() {
               </Button>
             </DropdownMenu.Trigger>
             <DropdownMenu.Content
-              className="w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800"
+              className="z-50 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800"
               sideOffset={8}
             >
               <DropdownMenu.Item
