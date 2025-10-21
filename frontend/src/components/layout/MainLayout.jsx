431e74f1a2666e182ddfc15365dc3fa6ba562327
xiez <xiez1989@gmail.com>
2025-10-21 18:32:16 +0800

    fix: fix layout

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -17,12 +17,12 @@ function Layout() {
       )}
     >
       <Sidebar />
-      <div className="flex flex-col">
+      <div className="grid grid-rows-[auto_1fr] overflow-hidden">
         <Header />
-        <main className="flex-1 overflow-auto p-4 md:p-6">
+        <main className="overflow-auto p-4 md:p-6">
           <Outlet />
         </main>
-      </div>
+      </div>      
     </div>
   );
 }
