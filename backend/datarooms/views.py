b79386576346d3bdca467632e29bd36ee8638f56
xiez <xiez1989@gmail.com>
2025-10-26 09:59:16 +0000

    feat: Implement datarooms API endpoint with serializers and viewsets

--- backend/datarooms/views.py
+++ backend/datarooms/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .models import Dataroom
+from .serializers import DataroomSerializer
+
+
+class DataroomViewSet(viewsets.ModelViewSet):
+    queryset = Dataroom.objects.all()
+    serializer_class = DataroomSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        This queryset ensures that users can only access datarooms
+        within their organization.
+        """
+        return self.queryset.filter(organization=self.request.user.organization)
+
+    def perform_create(self, serializer):
+        """
+        Automatically assign the organization and creator from the request user.
+        """
+        serializer.save(
+            organization=self.request.user.organization,
+            created_by=self.request.user
+        )
