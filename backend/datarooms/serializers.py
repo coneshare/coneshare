08f814d091ec38199c1d3e39073f2f8c31a0a620
xiez <xiez1989@gmail.com>
2025-11-09 04:31:15 +0000

    chore: Remove unused import in datarooms serializers

--- backend/datarooms/serializers.py
+++ backend/datarooms/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 
-# from documents.models import ShareLinkDataroomSetting
 from .models import Dataroom, DataroomDocument, DataroomFolder, ShareLinkDataroomSetting
 
 
